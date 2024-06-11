af4db5c31c9200a6f58ca4e3464f2a12997e0752
Moses <mail@moseswynn.com>
2024-06-11 19:15:08 -0400

    add s3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
 terraform {
+    backend "s3" {
+        bucket="mwtf"
+        key="terraform/ecs-pipelines"
+        region="us-west-2"
+    }
     required_providers {
         aws = {
             source = "hashicorp/aws"
